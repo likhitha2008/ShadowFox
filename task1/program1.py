f9ed167faa6718918b898e71c354cf900f636f33
likhitha2008 <likhithasrinivas118@gmail.com>
2026-05-09 17:55:00 +0530

    Update program1.py

--- task1/program1.py
+++ task1/program1.py
@@ -1,1 +1,16 @@
+#1
+pi=22/7
+print("value of pi:",pi)
+print("data type of pi",type(pi))
+#2
+# for=4 causes syntax error because for is keyword
+#print(for)
+num=4
+print("value",num)
+#3
 
+principle_amount=1000
+r=5
+t=3
+si=(p*t*r)/100
+print("simple interest",si)
